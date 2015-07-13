691ebf66a80733e2e2a6eb7eb3fe50fe5227147c
Sameer Rahmani <lxsameer@gnu.org>
2015-07-13 13:33:44 +0430

    convert 2 digits added.

--- lib/numerals_to_words.rb
+++ lib/numerals_to_words.rb
@@ -1,9 +1,29 @@
 require "numerals_to_words/version"
+require "numerals_to_words/constants"
+
 
 module NumeralsToWords
 
   def to_word
+    convert_2_digits_number(self)
   end
+
+  private
+
+    def convert_2_digits_number(value)
+
+      return LESS_THAN_20[value] if value < 20
+
+      0.upto(ALL_TENS.length) do |i|
+        word = ALL_TENS[i]
+        new_value = 20 + 10 * i
+
+        if new_value + 10 > value
+          return "#{word}-#{LESS_THAN_20[value % 10]}" if value % 10 > 0
+          return word
+        end
+      end
+    end
 
 end
 
---
+++ lib/numerals_to_words/constants.rb
@@ -0,0 +1,17 @@
+module NumeralsToWords
+  LESS_THAN_20 = ["zero",  "one",   "two",  "three", "four",   "five",   "six",
+                  "seven", "eight", "nine", "ten",   "eleven", "twelve",
+                  "thirteen", "fourteen", "fifteen", "sixteen", "seventeen",
+                  "eighteen", "nineteen" ]
+
+  ALL_TENS = ["twenty", "thirty", "forty", "fifty", "sixty", "seventy",
+              "eighty", "ninety"]
+
+  MORE_THAN_THOUSAND = ["", "thousand", "million", "billion", "trillion",
+                        "quadrillion", "quintillion", "sextillion", "septillion",
+                        "octillion", "nonillion", "decillion", "undecillion",
+                        "duodecillion", "tredecillion", "quattuordecillion",
+                        "sexdecillion", "septendecillion", "octodecillion",
+                        "novemdecillion", "vigintillion"]
+
+end
