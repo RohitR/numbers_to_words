b9b498497f560eade1868cd7a023d5c74df6c4af
Sameer Rahmani <lxsameer@gnu.org>
2015-07-13 14:00:07 +0430

    convertion for numbers bigger than thousand added.

--- lib/numerals_to_words.rb
+++ lib/numerals_to_words.rb
@@ -7,15 +7,33 @@ module NumeralsToWords
   def to_word
     return convert_2_digits_number(self) if self < 100
     return convert_3_digits_number(self) if self < 1000
+
+    MORE_THAN_THOUSAND.length.times do |i|
+      prev_prefix_index = i - 1
+
+      squared_val = 1000 ** i
+
+      if squared_val > self
+        prev_squared   = 1000 ** prev_prefix_index
+        left           = self / prev_squared
+        right          = self - (left * prev_squared)
+        thousand_value = MORE_THAN_THOUSAND[prev_prefix_index]
+
+        result = "#{convert_3_digits_number(left)} #{thousand_value}"
+        result = "#{result} #{right.to_word}" if right > 0
+        return result
+      end
+    end
   end
 
   private
+
 
     def convert_2_digits_number(value)
 
       return LESS_THAN_20[value] if value < 20
 
-      0.upto(ALL_TENS.length) do |i|
+      ALL_TENS.length.times do |i|
         word = ALL_TENS[i]
         new_value = 20 + 10 * i
 
